c6523c46629c1237b70ed1b2745aa86454b1c8ea
Matt1218 <welfleym@gmail.com>
2022-08-27 15:16:02 -0400

    initial commit

---
+++ src/main/java/com/welfley/issuetrackerbackend/IssuetrackerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.welfley.issuetrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuetrackerbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuetrackerbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/welfley/issuetrackerbackend/IssuetrackerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welfley.issuetrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuetrackerbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
